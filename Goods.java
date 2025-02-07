6039c1dafa2262a0148c62b5153cfbcd6f406c13
Utsav-8 <utsavraj.jarvis@gmail.com>
2025-02-07 15:18:49 +0530

    Update Goods.java

--- Goods.java
+++ Goods.java
@@ -1,5 +1,5 @@
-public class Main(){
-  public staticb void main(String args[]) {
+class Goods{
+  public static void main(String args[]) {
     System.out.println("This Programe is for Goods purchase in purchase module.");
   }  
 }  
